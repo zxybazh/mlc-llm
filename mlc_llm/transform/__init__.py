c4ed20be1d0e097b16b6887c0bd073aab94d9dcb
Eric Lunderberg <Lunderberg@users.noreply.github.com>
2023-05-17 17:57:50 -0500

    Allow non-aligned inputs to the model (#6)
    This allows avoiding copies between the pytorch and TVM portions of
    the `tests/benchmark.py` script.  Otherwise, the alignment requirement
    prevents passing `tvm.nd.from_dlpack(tokens[:, prev:current])` as an
    input to the model.

--- mlc_llm/transform/__init__.py
+++ mlc_llm/transform/__init__.py
@@ -3,4 +3,5 @@ from .lift_tir_global_buffer_alloc import LiftTIRGlobalBufferAlloc
 from .quantization import GroupQuantize
 from .transpose_matmul import FuseTransposeMatmul
 from .decode_matmul_ewise import FuseDecodeMatmulEwise
+from .allow_nonaligned_inputs import AllowNonAlignedInputs
 from .rewrite_attention import rewrite_attention
