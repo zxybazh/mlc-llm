fix logprobs with zero dimension

serve/mlc_serve/model/paged_cache_model.py
@@ -154,8 +154,7 @@ def sample(
         return None
 
     res_random = torch.multinomial(probs, 1, True).cpu().numpy()[:, 0]
-    # TODO(vvchernov): check logprobs shape
-    res_random_logprobs = torch.gather(logprobs.reshape(-1), dim=-1, index=torch.tensor(res_random, dtype=torch.int64, device=logits.device)).cpu().numpy()
+    res_random_logprobs = torch.gather(logprobs, dim=-1, index=torch.tensor(res_random, dtype=torch.int64, device=logits.device)).cpu().numpy()
 
     if logits_random.shape[0] == num_seq:
         return res_random, (res_random_logprobs, (top_random, top_random_logprob))