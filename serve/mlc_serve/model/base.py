d2875c2221329d307b79cca0b8ef54250f27d3d5
Jared Roesch <roeschinc@gmail.com>
2024-01-17 20:49:14 -0800

    Add structured error type when asset does not exist. (#165)
    * Add structured error type when asset does not exist.
    
    * Update error to contain path

--- serve/mlc_serve/model/base.py
+++ serve/mlc_serve/model/base.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
-import os 
+import os
 import json
 import inspect
 
@@ -31,16 +31,22 @@ class ModelArtifactConfig:
             }
         )
 
+class AssetNotFound(Exception):
+    def __init__(self, asset_path):
+        self.asset_path = asset_path
+        super().__init__(f"{self.asset_path} should exist. Did you build with `--enable-batching`?")
+
 def get_model_artifact_config(model_artifact_path):
     json_object = {"model_artifact_path": model_artifact_path}
-    for config_file_name in [ 
+    for config_file_name in [
         "build_config.json",
         "model/mlc-model-config.json"
     ]:
         config_file_path = os.path.join(model_artifact_path, config_file_name)
-        assert os.path.exists(config_file_path), f"{config_file_path} should exist. Did you build with `--enable-batching`?"
+        if not os.path.exists(config_file_path):
+            raise AssetNotFound(config_file_path)
+
         with open(config_file_path, mode="rt", encoding="utf-8") as f:
             json_object.update(json.load(f))
-    
+
     return ModelArtifactConfig._from_json(json_object)
-    
